0670481c1429c05fbe0df4abe0d71d55fe3f731a
choihooo <hochoi8621@naver.com>
2025-07-24 06:27:35 +0900

    Fix image import issues in LogoSection and VisualSection

--- src/components/LogoSection.tsx
+++ src/components/LogoSection.tsx
@@ -1,9 +1,7 @@
-import logo from "/logo.png";
-
 const LogoSection = () => {
   return (
     <div className="flex justify-center items-center w-full h-[180px]">
-      <img src={logo} alt="logo" />
+      <img src="/logo.png" alt="logo" />
     </div>
   );
 };
--- src/components/VisualSection.tsx
+++ src/components/VisualSection.tsx
@@ -1,9 +1,7 @@
-import visual from "/visual.jpg";
-
 const VisualSection = () => {
   return (
     <div className="flex justify-center items-center w-full h-[180px]">
-      <img src={visual} alt="visual" />
+      <img src="/visual.jpg" alt="visual" />
     </div>
   );
 };
